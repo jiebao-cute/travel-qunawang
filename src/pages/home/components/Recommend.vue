a5738f365b031e05f4bbc5f52f6eb689e50bd81d
jiebao <908822571@qq.com>
2021-05-27 22:50:46 +0800

    item溢出的文字省略，不现在在容器上加min-width:0

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -6,7 +6,7 @@
         <div class="item border-bottom">
           <img class="item-img" src="https://imgs.qunarzz.com/wugc/p224/201207/13/f40f8a9e4327658c93835fbb.jpg_200x200_484bc04f.jpg" alt=""/>
           <div class="info">
-          <p class="item-title">成都大熊猫繁育研究基地</p>
+          <p class="item-title">成都大熊猫繁育研究基地成都大熊猫繁育研究基地成都大熊猫繁育研究基地</p>
           <p class="item-desc">看！熊猫宝宝在向你招手呢</p>
             <button class="item-button">查看详情</button>
           </div>
@@ -23,6 +23,7 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import "~@/assets/styles/mixins.styl"
  .title{
    background :#cacaca
    line-height: .8rem
@@ -41,13 +42,16 @@ export default {
        .info{
          flex: 1
          padding: .1rem
+         min-width: 0
          .item-title{
            line-height: .54rem
-            font-size: .32rem
+           font-size: .32rem
+           ellipsis()
          }
          .item-desc{
-          line-height: .44rem
+           line-height: .44rem
            color: #cccccc
+           ellipsis()
          }
          .item-button{
           line-height:  .48rem
