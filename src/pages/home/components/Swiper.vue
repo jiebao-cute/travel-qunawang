c4ec55bbaec3890b06e36a331b53771343ec9e60
jiebao <908822571@qq.com>
2021-05-27 17:42:28 +0800

    修改格式

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -16,19 +16,18 @@ export default {
   data () {
     return {
       swiperOptions: {
-        pagination: '.swiper-pagination', //将点传播出去
-        loop: true, //循环轮播
-        autoplay: true,//自动轮播
-        speed: 3000//轮播时间
+        pagination: '.swiper-pagination', //  将点传播出去
+        loop: true, //  循环轮播
+        autoplay: true, //  自动轮播
+        speed: 3000 //  轮播时间
       },
       swiperList: [{
         id: '0001',
-        imgUrl: "https://imgs.qunarzz.com/vs_ceph_vcimg/c77891ece9fb3abd9c7156f1588f5732.jpeg"
-      },
-        {
-          id: '0002',
-          imgUrl: "https://imgs.qunarzz.com/vs_ceph_vcimg/f8bd77048382f0a8ab109f860f04a88f.jpeg"
-        }
+        imgUrl: 'https://imgs.qunarzz.com/vs_ceph_vcimg/c77891ece9fb3abd9c7156f1588f5732.jpeg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://imgs.qunarzz.com/vs_ceph_vcimg/f8bd77048382f0a8ab109f860f04a88f.jpeg'
+      }
       ]
     }
   }
