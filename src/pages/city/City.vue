88d1d99a89ec6ac952c0efa675e1b23638697cdd
jiebao <908822571@qq.com>
2021-06-01 16:16:41 +0800

    城市页面输入框的样式和input组件完成

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-  <city-header></city-header>
+    <city-header></city-header>
+    <city-search></city-search>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header.vue'
+import CitySearch from './components/Search.vue'
 export default {
-
   name: 'City',
-  components:{ CityHeader}
-
+  components:{ CityHeader, CitySearch}
 }
 </script>
 <style  lang="stylus" scoped>
