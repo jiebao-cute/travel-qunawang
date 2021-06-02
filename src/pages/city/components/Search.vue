f6b2e284c992923e6ca7c1136177bb7c136e66f5
jiebao <908822571@qq.com>
2021-06-02 17:16:07 +0800

    keyword输入的有问题的时候，显示没有匹配的城市

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -3,9 +3,10 @@
   <div class="search" >
    <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音"/>
   </div>
-  <div class="search-content" ref="search">
+  <div class="search-content" ref="search" >
     <ul>
       <li class="search-item border-bottom" v-for="item in list" :key="item.id">{{item.name}}</li>
+      <li class="search-item border-bottom" v-show="showWord">没有匹配的城市</li>
     </ul>
   </div>
   </div>
@@ -23,6 +24,11 @@ export default {
       keyword: '',
       list: [],
       timer: null
+    }
+  },
+  computed:{
+    showWord () {
+      return !this.list.length
     }
   },
   mounted () {
