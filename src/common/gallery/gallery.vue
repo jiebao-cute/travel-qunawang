41fd7230d6c12dfc4a908b5e6907752742af0d3a
jiebao <908822571@qq.com>
2021-06-04 15:57:29 +0800

    實現橫向拖動圖片

--- src/common/gallery/gallery.vue
+++ src/common/gallery/gallery.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="container">
-    <div class="wrapper"> 123</div>
+    <div class="wrapper">
+      <swiper :options="swiperOptions">
+        <swiper-slide>
+          <img class="gallery-img" src="http://img1.qunarzz.com/sight/p0/1501/40/40b2b6c951b28fdd.water.jpg_140x140_1c863e5c.jpg" alt="slow"/>
+        </swiper-slide>
+        <swiper-slide>
+          <img class="gallery-img" src="http://img1.qunarzz.com/sight/p0/1501/40/40b2b6c951b28fdd.water.jpg_140x140_1c863e5c.jpg" alt="slow"/>
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
     gallery
   </div>
 </template>
 <script>
 export default {
-  name: 'CommonGallery'
+  name: 'CommonGallery',
+  data () {
+    return {
+      swiperOptions: {
+        paginationType : 'fraction',
+      }
+    }
+  },
 }
 </script>
 <style lang="stylus" scoped>
@@ -22,7 +39,13 @@ export default {
   right: 0
   background: #000000
   .wrapper{
-    background: #eeeeee
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 100%
+    .gallery-img{
+      width: 100%
+    }
   }
 }
 </style>
