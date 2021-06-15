0906529e74449f3e5d0f60de85ed66b1876456ff
jiebao <908822571@qq.com>
2021-06-15 19:37:51 +0800

    header样式初步完成

---
+++ src/pages/Detail/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <router-link to="/">
+  <div class="header-abc">
+    <div class="iconfont header-icon-abs">&#xe60e;</div>
+  </div>
+    </router-link>
+  <div class="header-fixed">景点详情
+    <router-link to="/">
+      <div class="iconfont header-fixer-back">&#xe60e;</div>
+    </router-link>
+  </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'DetailHeader'
+}
+</script>
+<style lang="stylus" scoped>
+@import "~@/assets/styles/varibles.styl"
+.header-abc {
+  position: absolute
+  left: .2rem
+  top: .2rem
+  width .8rem
+  height: .8rem
+  border-radius: .4rem
+  line-height: .8rem
+  text-align: center
+  background: rgb(0,0,0,.8)
+  .header-icon-abs{
+    color: #ffffff
+    font-size: .4rem
+  }
+}
+.header-fixed {
+  position: fixed
+  top: 0
+  left: 0
+  right: 0
+  overflow: hidden
+  background: $bgColor
+  height: $headerHeight
+  line-height: $headerHeight
+  color: white
+  text-align: center
+  font-size: .34rem
+  .header-fixer-back {
+    box-sizing: border-box
+    position: absolute
+    top: 0
+    left: 0
+    width: .64rem
+    text-align: center
+    font-size: .4rem
+    color: #ffffff
+  }
+}
+</style>
