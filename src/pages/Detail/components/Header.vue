2b154d342db3477f9dc5cadefddf1f620dd2b749
jiebao <908822571@qq.com>
2021-06-16 16:17:25 +0800

    解决header不在最顶层问题

--- src/pages/Detail/components/Header.vue
+++ src/pages/Detail/components/Header.vue
@@ -26,7 +26,6 @@ export default {
   methods: {
     handleScroll () {
       const top = document.documentElement.scrollTop
-      console.log(top)
       if (top > 60) {
         let opacity = top / 140
         opacity = opacity > 1 ? 1 : opacity
@@ -63,6 +62,7 @@ export default {
   }
 }
 .header-fixed {
+  z-index: 2
   position: fixed
   top: 0
   left: 0
